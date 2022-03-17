f36140e7d1f40ea21c79f211a6dc76c480df24fe
Romain Coumans <romain@ceracarbon-racing.com>
2022-03-17 23:36:30 +0100

    Simulatie verplaatst en aangepast Verschillende experimenten kunnen nu makkelijk uitgevoerd worden.

---
+++ nieuw_model/experiment_0.py
@@ -0,0 +1,35 @@
+"""
+Dit experiment bestand runt de simulatie met de parameters zoals origineel aangeleverd.
+"""
+import numpy as np
+import simulatie_v2
+
+tmax = 36000
+dt = 0.1
+
+def X_func(t):
+    global tmax
+    t_control = np.array([0, 0.1*tmax, 0.2*tmax, 0.5*tmax,
+                         0.6*tmax, 0.7*tmax, tmax])
+    X_parms = np.array([0.85, 0.85, 0.3, 0.4, 0.4, 1, 1])
+
+    return np.interp(t, t_control, X_parms)
+
+def Y_func(t):
+    global tmax
+    t_control = np.array([0, 0.1*tmax, 0.2*tmax, 0.5*tmax,
+                         0.6*tmax, 0.7*tmax, tmax])
+    Y_parms = np.array([1, 1, 1, 1, 1, 1, 1])
+
+    return np.interp(t, t_control, Y_parms)
+
+def clutch_func(t, X, n_e):
+    """
+    Functie die bepaalt of de motor wel of niet moet worden ontkoppelt. Kan op tijdsbasis of basis van andere
+    parameters.
+    """
+    if X < 0.21 and n_e < 300/60:
+        return False
+    return True
+
+simulatie_v2.simulation(X_func, Y_func, clutch_func, tmax, dt, "grafieken/exp0/")
